461c557a356317b389bc622dacbffc69557f3e50
Matheus Andrade Alves <10518216+mtwalves@users.noreply.github.com>
2022-12-03 12:11:07 -0300

    Add paddings and change text and button size

--- lib/home.dart
+++ lib/home.dart
@@ -15,7 +15,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late Timer timer;
-  Pomodoro pomodoro = Pomodoro(pomodoroDuration: const Duration(minutes: 1));
+  Pomodoro pomodoro = Pomodoro();
 
   @override
   void initState() {
@@ -43,56 +43,72 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       backgroundColor: Colors.redAccent,
       body: Center(
         child: Container(
           color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ToggleButtons(
-                  isSelected: [
-                    pomodoro.state == PomodoroState.pomodoro,
-                    pomodoro.state == PomodoroState.shortBreak,
-                    pomodoro.state == PomodoroState.longBreak,
-                  ],
-                  onPressed: (int index) {
-                    setState(() {
-                      pomodoro.state = PomodoroState.values[index];
-                    });
-                  },
-                  borderRadius: const BorderRadius.all(Radius.circular(8)),
-                  selectedBorderColor: Colors.red[700],
-                  selectedColor: Colors.white,
-                  fillColor: Colors.red[200],
-                  color: Colors.red[400],
-                  constraints: const BoxConstraints(
-                    minHeight: 40.0,
-                    minWidth: 80.0,
-                  ),
-                  children: const [
-                    Text('Pomodoro'),
-                    Text('Short Break'),
-                    Text('Long Break')
-                  ]),
-              Text(
-                remainingTime,
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (pomodoro.isRunning) {
-                      pomodoro.stop();
-                    } else {
-                      pomodoro.start();
-                    }
-                  },
-                  child: Text(pomodoro.isRunning ? 'Stop' : 'Start')),
-              Text('#${pomodoro.pomodorosCompleted}')
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 50,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ToggleButtons(
+                    isSelected: [
+                      pomodoro.state == PomodoroState.pomodoro,
+                      pomodoro.state == PomodoroState.shortBreak,
+                      pomodoro.state == PomodoroState.longBreak,
+                    ],
+                    onPressed: (int index) {
+                      setState(() {
+                        pomodoro.state = PomodoroState.values[index];
+                      });
+                    },
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                    selectedBorderColor: Colors.red[700],
+                    selectedColor: Colors.white,
+                    fillColor: Colors.red[200],
+                    color: Colors.red[400],
+                    constraints: const BoxConstraints(
+                      minHeight: 40.0,
+                      minWidth: 80.0,
+                    ),
+                    children: const [
+                      Text('Pomodoro'),
+                      Text('Short Break'),
+                      Text('Long Break')
+                    ]),
+                const SizedBox(height: 20),
+                Text(
+                  remainingTime,
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      if (pomodoro.isRunning) {
+                        pomodoro.stop();
+                      } else {
+                        pomodoro.start();
+                      }
+                    },
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 50),
+                      ),
+                    ),
+                    child: Text(
+                      pomodoro.isRunning ? 'STOP' : 'START',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    )),
+                const SizedBox(height: 20),
+                Text('#${pomodoro.pomodorosCompleted}')
+              ],
+            ),
           ),
         ),
       ),
