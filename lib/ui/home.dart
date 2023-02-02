ac27466b9f78edb7c6436aa66963bcae2aef3dbb
Matheus Andrade Alves <10518216+mtwalves@users.noreply.github.com>
2023-02-02 17:17:29 -0300

    Use provider package to consume Pomodoro

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../core/pomodoro.dart';
 
@@ -15,7 +16,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late Timer timer;
-  Pomodoro pomodoro = Pomodoro();
 
   @override
   void initState() {
@@ -32,7 +32,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  String get remainingTime {
+  // TODO: Add tests for this function (bug when minutes > 60)
+  String remainingTime(Pomodoro pomodoro) {
     Duration remaining = pomodoro.remainingTime;
     String minutes = (remaining.inMinutes % 60).toString().padLeft(2, '0');
     String seconds = (remaining.inSeconds % 60).toString().padLeft(2, '0');
@@ -44,70 +45,72 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.redAccent,
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 20,
-              horizontal: 50,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ToggleButtons(
-                    isSelected: [
-                      pomodoro.state == PomodoroState.pomodoro,
-                      pomodoro.state == PomodoroState.shortBreak,
-                      pomodoro.state == PomodoroState.longBreak,
-                    ],
-                    onPressed: (int index) {
-                      setState(() {
-                        pomodoro.state = PomodoroState.values[index];
-                      });
-                    },
-                    borderRadius: const BorderRadius.all(Radius.circular(8)),
-                    selectedBorderColor: Colors.red[700],
-                    selectedColor: Colors.white,
-                    fillColor: Colors.red[200],
-                    color: Colors.red[400],
-                    constraints: const BoxConstraints(
-                      minHeight: 40.0,
-                      minWidth: 80.0,
-                    ),
-                    children: const [
-                      Text('Pomodoro'),
-                      Text('Short Break'),
-                      Text('Long Break')
-                    ]),
-                const SizedBox(height: 20),
-                Text(
-                  remainingTime,
-                  style: Theme.of(context).textTheme.displayLarge,
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                    onPressed: () {
-                      if (pomodoro.isRunning) {
-                        pomodoro.stop();
-                      } else {
-                        pomodoro.start();
-                      }
-                    },
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all<EdgeInsets>(
-                        const EdgeInsets.symmetric(
-                            vertical: 20, horizontal: 50),
+      body: Consumer<Pomodoro>(
+        builder: (context, pomodoro, child) => Center(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 50,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ToggleButtons(
+                      isSelected: [
+                        pomodoro.state == PomodoroState.pomodoro,
+                        pomodoro.state == PomodoroState.shortBreak,
+                        pomodoro.state == PomodoroState.longBreak,
+                      ],
+                      onPressed: (int index) {
+                        setState(() {
+                          pomodoro.state = PomodoroState.values[index];
+                        });
+                      },
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                      selectedBorderColor: Colors.red[700],
+                      selectedColor: Colors.white,
+                      fillColor: Colors.red[200],
+                      color: Colors.red[400],
+                      constraints: const BoxConstraints(
+                        minHeight: 40.0,
+                        minWidth: 80.0,
                       ),
-                    ),
-                    child: Text(
-                      pomodoro.isRunning ? 'STOP' : 'START',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    )),
-                const SizedBox(height: 20),
-                Text('#${pomodoro.pomodorosCompleted}')
-              ],
+                      children: const [
+                        Text('Pomodoro'),
+                        Text('Short Break'),
+                        Text('Long Break')
+                      ]),
+                  const SizedBox(height: 20),
+                  Text(
+                    remainingTime(pomodoro),
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (pomodoro.isRunning) {
+                          pomodoro.stop();
+                        } else {
+                          pomodoro.start();
+                        }
+                      },
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          const EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 50),
+                        ),
+                      ),
+                      child: Text(
+                        pomodoro.isRunning ? 'STOP' : 'START',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      )),
+                  const SizedBox(height: 20),
+                  Text('#${pomodoro.pomodorosCompleted}')
+                ],
+              ),
             ),
           ),
         ),
