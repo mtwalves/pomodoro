74962763aed931097cbf069d2e7eb6da3e8b0bdc
Matheus Andrade Alves <10518216+mtwalves@users.noreply.github.com>
2023-02-20 16:31:12 -0300

    Change bloc to hydrated to save state

--- lib/core/bloc/pomodoro/bloc/pomodoro_state.dart
+++ lib/core/bloc/pomodoro/bloc/pomodoro_state.dart
@@ -1,16 +1,81 @@
 part of 'pomodoro_bloc.dart';
 
 abstract class PomodoroState extends Equatable {
-  const PomodoroState();
+  const PomodoroState({
+    required this.pomodoroDuration,
+    required this.shortBreakDuration,
+    required this.longBreakDuration,
+    required this.longBreakInterval,
+    required this.pomodorosCompleted,
+  });
+
+  final Duration pomodoroDuration;
+  final Duration shortBreakDuration;
+  final Duration longBreakDuration;
+  final int longBreakInterval;
+  final int pomodorosCompleted;
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [
+        pomodoroDuration.inSeconds,
+        shortBreakDuration.inSeconds,
+        longBreakDuration.inSeconds,
+        longBreakInterval
+      ];
 }
 
-class PomodoroInitial extends PomodoroState {}
+class PomodoroInitial extends PomodoroState {
+  const PomodoroInitial({
+    required super.pomodoroDuration,
+    required super.shortBreakDuration,
+    required super.longBreakDuration,
+    required super.longBreakInterval,
+    required super.pomodorosCompleted,
+  });
 
-class PomodoroSet extends PomodoroState {}
+  @override
+  String toString() =>
+      'PomodoroInitial { pomodoroDuration: ${pomodoroDuration.inSeconds} }';
+}
 
-class PomodoroShortBreak extends PomodoroState {}
+class PomodoroSet extends PomodoroState {
+  const PomodoroSet({
+    required super.pomodoroDuration,
+    required super.shortBreakDuration,
+    required super.longBreakDuration,
+    required super.longBreakInterval,
+    required super.pomodorosCompleted,
+  });
 
-class PomodoroLongBreak extends PomodoroState {}
+  @override
+  String toString() =>
+      'PomodoroSet { pomodoroDuration: ${pomodoroDuration.inSeconds} }';
+}
+
+class PomodoroShortBreak extends PomodoroState {
+  const PomodoroShortBreak({
+    required super.pomodoroDuration,
+    required super.shortBreakDuration,
+    required super.longBreakDuration,
+    required super.longBreakInterval,
+    required super.pomodorosCompleted,
+  });
+
+  @override
+  String toString() =>
+      'PomodoroShortBreak { pomodoroDuration: ${pomodoroDuration.inSeconds} }';
+}
+
+class PomodoroLongBreak extends PomodoroState {
+  const PomodoroLongBreak({
+    required super.pomodoroDuration,
+    required super.shortBreakDuration,
+    required super.longBreakDuration,
+    required super.longBreakInterval,
+    required super.pomodorosCompleted,
+  });
+
+  @override
+  String toString() =>
+      'PomodoroLongBreak { pomodoroDuration: ${pomodoroDuration.inSeconds} }';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:pomodoro/core/bloc/pomodoro/bloc/pomodoro_bloc.dart';
 import 'package:pomodoro/core/bloc/timer/ticker.dart';
 import 'package:pomodoro/core/bloc/timer/timer_bloc.dart';
@@ -7,7 +10,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'ui/home.dart';
 import 'ui/settings.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+
   runApp(MyApp());
 }
 
